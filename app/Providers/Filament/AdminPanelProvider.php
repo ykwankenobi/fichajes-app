2fec189e3f93259da703c495faa72815b780d86b
ykwankenobi <263320743+ykwankenobi@users.noreply.github.com>
2026-08-31 15:15:52 +0000

    Show attendance calendar first on dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -56,8 +56,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                AdminStatsOverview::class,
                 AttendanceCalendar::class,
+                AdminStatsOverview::class,
             ])
             ->middleware([
                 EncryptCookies::class,
